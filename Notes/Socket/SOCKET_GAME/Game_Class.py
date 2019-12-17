90356e87ec2239c2ca83dc46cee287f245f052b2
rp <rp729@yahoo.com>
2019-12-16 21:09:23 -0600

    Working Copy

--- Notes/Socket/SOCKET_GAME/Game_Class.py
+++ Notes/Socket/SOCKET_GAME/Game_Class.py
@@ -2,12 +2,12 @@
 https://www.journaldev.com/15911/python-super
 '''
 import socket
+import random
 
 class NetConnect:
-    def __init__(self,hostname,port,message=bytes("",encoding="utf-8")):
+    def __init__(self,hostname,port):
         self.hostname = hostname
         self.port = port
-        self.message = message
 
     def set_hostname(self,hostname):
         self.hostname = hostname
@@ -16,7 +16,7 @@ class NetConnect:
         self.port = port
 
     def set_message(self,message):
-        self.message = message
+        self.message = bytes(message, encoding="utf-8")
 
     def get_hostname(self):
         return self.hostname
@@ -25,41 +25,86 @@ class NetConnect:
         return self.port
 
     def get_message(self):
-        return self.message
+        if self.message:
+            return self.message
+        else:
+            return
 
-class SendPkt(NetConnect):
-    def __init__(self,hostname,port,message=None):
-        super().__init__(hostname,port,message)
+class NetClient(NetConnect):
+    def __init__(self,hostname,port):
+        super().__init__(hostname,port)
     def send_packet(self):
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((self.hostname,int(self.port)))
-        s.sendall((self.message))
-        s.shutdown(socket.SHUT_WR)
-        while 1:
-            data = s.recv(1024)
-            if data == "":
+        print("Get ready to ride the bull!")
+        while True:
+            guess = input("Enter number: ")
+            s.send(guess.encode())
+            data = s.recv(80).decode()
+            print(data)
+            if data == "found the secret":
                 break
             print("Received:",repr(data))
-            message = input("Enter message: ")
         print("Connection closed.")
         s.close()
 
+class NetServer(NetConnect):
+    def __init__(self,hostname,port):
+        super().__init__(hostname, port)
+    def secret(self):
+        secret_num = random.randint(1,10)
+        print(f"The secret number is {secret_num}")
+        return secret_num
+    def connect(self):
+        num = self.secret()
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #Find difference of bind and connect
+        s.bind((self.hostname,int(self.port)))
+        #What does the 1 stand for?
+        s.listen(1)
+        socket_,address = s.accept()
+        #print(socket_)
+        print(f"Connection establisghed from {address}")
+        while True:
+            print('Dealer waits for a guess')
+            guess = socket_.recv(80).decode()
+            print(f"Dealer received {guess}")
+            if int(guess) < num:
+                reply = 'too low'
+            elif int(guess) > num:
+                reply = 'too high'
+            else:
+                reply = 'found the secret'
+            socket_.send(reply.encode())
+            if reply == 'found the secret':
+                break
+        s.close()
+
+
 def main():
-    pkt_build = packet_build()
-    packet_send = SendPkt(pkt_build.get_hostname(),pkt_build.get_port(),pkt_build.get_message())
-    packet_send.send_packet()
+    print("Please choose from the following:\n"
+          "1) Player\n"
+          "2) Dealer")
+    user = input("Response: ")
+    user = input_validation(user)
+    if user == 1:
+        pkt = packet_build()
+        client = NetClient(pkt.get_hostname(),pkt.get_port())
+        client.send_packet()
+    elif user == 2:
+        pkt = packet_build()
+        server = NetServer(pkt.get_hostname(),pkt.get_port())
+        server.connect()
 
 def packet_build():
     hostname = input("Enter hostname: ")
     port = input("Enter port: ")
-    message = input("Would you like to add a message? (y for yes): ")
-    if message.lower() == 'y':
-        message = input("Enter message: ")
-        message = bytes(message,encoding='utf-8')
-    else:
-        message = bytes("",encoding="utf-8")
-    nc = NetConnect(hostname,port,message)
+    nc = NetConnect(hostname,port)
     return nc
 
+def input_validation(num):
+    while str.isnumeric(num) == False:
+        num = input("ERROR! Enter a number: ")
+    return int(num)
 
 main()
