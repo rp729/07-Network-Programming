6b949706c18acabe8cdddc6a1e551367265d72cf
rp <rp729@yahoo.com>
2019-12-17 20:32:35 -0600

    Working copy of full duplex but needs work

---
+++ Notes/Socket/Socket_Challenge_Questions/full_duplex_scratch.py
@@ -0,0 +1,89 @@
+'''
+https://www.journaldev.com/15911/python-super
+'''
+import socket
+import threading
+
+class NetConnect:
+    def __init__(self,hostname,port):
+        self.hostname = hostname
+        self.port = port
+
+    def set_hostname(self,hostname):
+        self.hostname = hostname
+
+    def set_message(self,message):
+        self.message = bytes(message, encoding="utf-8")
+
+    def get_hostname(self):
+        return self.hostname
+
+    def get_port(self):
+        return self.port
+
+    def get_message(self):
+        if self.message:
+            return self.message
+        else:
+            return
+    def send_message(self,sock):
+        while True:
+            new_message = input()
+            sock.send(new_message.encode())
+            print("Type message below:")
+
+    def receive_message(self,sock):
+        while True:
+            message = sock.recv(1024).decode()
+            print(f"User response ==>:{message}")
+
+    def net_client(self):
+        name = socket.gethostname()
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((self.hostname,int(self.port)))
+        name = s.recv(80).decode()
+        print("Begin typing message to start conversation.")
+        t1 = threading.Thread(target=self.send_message,args=[s,])
+        t2 = threading.Thread(target=self.receive_message,args=[s,])
+        t1.start()
+        t2.start()
+    def net_server(self):
+        print('Patiently waiting on client.')
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.bind((self.hostname,int(self.port)))
+        s.listen(1)
+        socket_,address = s.accept()
+        print(f'Connection established from {address}')
+        name = socket.gethostname()
+        socket_.send(name.encode())
+        print("Begin typing message to start conversation.")
+        t1 = threading.Thread(target=self.receive_message,args=[socket_,])
+        t2 = threading.Thread(target=self.send_message,args=[socket_,])
+        t1.start()
+        t2.start()
+
+
+
+def main():
+    user = input("1) Client \n2) Server \nResponse:")
+    user = input_validation(user)
+    if user == 1:
+        pkt = packet_build()
+        pkt.net_client()
+    elif user == 2:
+        pkt = packet_build()
+        pkt.net_server()
+
+def packet_build():
+    hostname = input("Enter hostname: ")
+    port = input("Enter port: ")
+    nc = NetConnect(hostname,port)
+    return nc
+
+def input_validation(num):
+    while str.isnumeric(num) == False:
+        num = input("ERROR! Enter a number: ")
+    return int(num)
+
+
+main()
