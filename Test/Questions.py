de123c0c453055bb9f8ea193795a27b38771d0d3
rp729 <rp729@yahoo.com>
2019-12-20 09:15:55 -0600

    test questions

---
+++ Test/Questions.py
@@ -0,0 +1,27 @@
+'''
+1) Write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read
+the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the
+sum of the numbers in the file and enter the sum below: Here are two files for this assignment. One is a sample file
+where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_57128.json (Sum ends with 10)
+You do not need to save these files to your folder since your program will read the data directly from the URL.
+===================================================================================================================
+2) Scraping Numbers from HTML using python modules. In this assignment you will write a Python program similar to
+http://www.py4e.com/code3/urllink2.py. The program will read the HTML from the data files below, and parse the data,
+extracting numbers and compute the sum of the numbers in the file.
+
+We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other
+is the actual data you need to process for the assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+
+Actual data: http://py4e-data.dr-chuck.net/comments_57125.html (Sum ends with 54)
+
+You do not need to save these files to your folder since your program will read the data directly from the URL.
+===================================================================================================================
+
+
+3) Write a client server python program to send input from client to server, replace all the vowels with '#' and return the string to client and print it
+'''
